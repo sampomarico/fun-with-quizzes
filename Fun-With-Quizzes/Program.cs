dd84f7d89d70c180c6c530fb752ecfacd9e3d72b
Sam <samtpomarico@gmail.com>
2020-10-20 19:08:43 -0500

    First commit. Created empty quiz classes

---
+++ Fun-With-Quizzes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fun_With_Quizzes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Fun-With-Quizzes/TrueOrFalse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fun_With_Quizzes
+{
+    class TrueOrFalse
+    {
+    }
+}
